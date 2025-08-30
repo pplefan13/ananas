fbfd26da4869cefb6309f0dc03ce3a3e247a0a80
pplefan13 <chirea.iulia@yahoo.com>
2025-08-30 21:24:18 +0300

    modified

--- functions.js
+++ functions.js
@@ -11,7 +11,8 @@ if(!data.names)
   data.names=[];
 if(!data.dates)
   data.dates=[];
-console.log(data.dates[0]));
+console.log(mi);
+console.log(data.dates[0]);
 
 let x=return_array(data.names, "c1");
 let y=return_array(data.dates, "c2");
