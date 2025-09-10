e6d0e097d7f00f2d2d23fb0e7193a52b698cde0f
pplefan13 <chirea.iulia@yahoo.com>
2025-09-10 22:56:14 +0300

    wifi credentials

--- src/ota/ota.h
+++ src/ota/ota.h
@@ -5,8 +5,8 @@ void ota_setup();
 void ota_loop();
 
 #ifndef STASSID
-#define STASSID "AndroidAP"
-#define STAPSK "laiu2345"
+#define STASSID "NetRDS ext"
+#define STAPSK "constructor"
 extern const char* ssid;
 extern const char* password;
 #endif
